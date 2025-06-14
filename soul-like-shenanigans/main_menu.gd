f3562fdc371d50ba0a4474c89979f26d0fcf44e4
Zapiltar-E7 <dylanjosephx@gmail.com>
2025-06-14 06:35:37 -0400

    b
    baisc

---
+++ soul-like-shenanigans/main_menu.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_new_game_pressed():
+	pass # Replace with function body.
+
+
+func _on_system_pressed():
+	pass # Replace with function body.
+
+
+func _on_exit_pressed():
+	get_tree().quit()
